2aa35e5f72eaf24baf25c8fa67fd60a8833b7ff5
Nickhayatun Firza Refiani <96812402+nfirzaaa@users.noreply.github.com>
2024-01-08 09:06:32 +0700

    update file

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-        <img src="{{ asset('template-admin/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{ asset('template-admin/dist/img/Logo_udinus1.jpg') }}" alt="AdminLTE Logo"
+            class="brand-image img-circle elevation-3" style="opacity: .9">
         <span class="brand-text font-weight-light">Polikllinik</span>
     </a>
 
@@ -11,7 +11,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ asset('template-admin/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                <img src="{{ asset('template-admin/dist/img/medis.jpg') }}" class="img-circle elevation-2"
                     alt="User Image">
             </div>
             <div class="info">
